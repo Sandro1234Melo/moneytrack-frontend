f54776171c8417ecae1f6b1d3cda512df7c9496f
Sandro1234Melo <108824465+Sandro1234Melo@users.noreply.github.com>
2026-01-22 22:46:02 +0000

    Fazendo alterações para o Deploy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import.meta.env.VITE_API_URL
 
 const api = axios.create({
-  baseURL: "https://localhost:7074/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
