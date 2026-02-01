cd4f2e368770dbd361afaf5fa3db0dc6963edfb8
Sandro1234Melo <108824465+Sandro1234Melo@users.noreply.github.com>
2026-02-01 22:05:47 +0000

    Incluido outra forma de Pagamento

--- src/utils/paymentMethods.ts
+++ src/utils/paymentMethods.ts
@@ -4,5 +4,6 @@ export const paymentMethods = [
   { value: 2, label: "Cartão Crédito" },
   { value: 3, label: "PIX" },
   { value: 4, label: "Transferência" },
-  { value: 5, label: "Mbway" }
+  { value: 5, label: "Mbway" },
+  { value: 6, label: "Vale Alimentação" }
 ];
